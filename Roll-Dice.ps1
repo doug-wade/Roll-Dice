6029e9fd96b9b6b2b17ea5649764f82e35612a9d
Douglas Wade <douglas.b.wade@gmail.com>
2020-02-21 15:32:32 -0800

    Add simple argument handling

--- Roll-Dice.ps1
+++ Roll-Dice.ps1
@@ -1,2 +1,13 @@
-﻿$rand = Get-Random -Minimum 1 -Maximum 21
-Write-Output $rand
+﻿if ($args.Length -gt 0)
+{
+    $argument_array = $args[0].split("d")
+    $number_of_dice = $argument_array[0]
+    $dice_type = $argument_array[1]
+    Write-Output "Rolling $($number_of_dice)d$($dice_type)"
+}
+else
+{
+    $rand = Get-Random -Minimum 1 -Maximum 21
+    Write-Output "default case"
+    Write-Output $rand
+}
